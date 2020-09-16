78f14de7f77b242fcf9042f7d06bf82323aeffcb
Tien Nguyen Xuan <chutien.it@gmail.com>
2020-09-16 14:14:41 +0700

    [ADD] add Stories progressbar for sameple ViewPagerActivity

--- core-sample-app/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/sampleapp/examples/viewPagerExample/ViewPagerActivity.java
+++ core-sample-app/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/sampleapp/examples/viewPagerExample/ViewPagerActivity.java
@@ -1,9 +1,13 @@
 package com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.viewPagerExample;
 
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.view.View;
 
 import com.pierfrancescosoffritti.aytplayersample.R;
 
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
@@ -11,19 +15,87 @@ import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ViewPagerActivity extends AppCompatActivity {
+import jp.shts.android.storiesprogressview.StoriesProgressView;
 
-    private static final int NUM_PAGES = 5;
+public class ViewPagerActivity extends AppCompatActivity implements StoriesProgressView.StoriesListener, VideoListener {
+    private static Pair<String,String>[] videos = new Pair[]{new Pair<>("0Xcfw3bQC28", "Người máy"), new Pair<>("ic_QQHY836Y", "Thời trang"),
+            new Pair<>("DJ1lH4wFXeg", "Trượt tuyết"), new Pair<>("66gxb2QxlGk", "Quảng cáo adidas")};
+    private StoriesProgressView storie_progress_bar;
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
+    private VideoListener videoListener = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pager_example);
 
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        PagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager = findViewById(R.id.view_pager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(viewPagerAdapter);
+        viewPager.setOffscreenPageLimit(2);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                storie_progress_bar.startStories(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        initProgressBar();
     }
+
+    private void initProgressBar() {
+        storie_progress_bar = findViewById(R.id.storie_progress_bar);
+        long[] storiesDuration = {15000L, 16000L, 42000L, 45000L};
+        storie_progress_bar.setStoriesCountWithDurations(storiesDuration);
+        storie_progress_bar.setStoriesListener(this);
+
+    }
+
+    @Override
+    public void onNext() {
+        int currentItem = viewPager.getCurrentItem();
+        int count = viewPagerAdapter.getCount();
+        if (currentItem < count - 1) {
+            viewPager.setCurrentItem(currentItem + 1);
+        }
+    }
+
+    @Override
+    public void onPrev() {
+        int currentItem = viewPager.getCurrentItem();
+        if (currentItem > 0) {
+            viewPager.setCurrentItem(currentItem - 1);
+        }
+    }
+
+    @Override
+    public void onComplete() {
+
+    }
+
+    @Override
+    public void ready() {
+        storie_progress_bar.startStories();
+    }
+    @Override
+    public void pause() {
+        storie_progress_bar.pause();
+    }
+    @Override
+    public void resume() {
+        storie_progress_bar.resume();
+    }
+
 
     private class ViewPagerAdapter extends FragmentStatePagerAdapter {
         ViewPagerAdapter(FragmentManager fm) {
@@ -32,17 +104,22 @@ public class ViewPagerActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            return new ViewPagerFragment();
+            return ViewPagerFragment.newInstance(videos[position].first, videos[position].second);
         }
 
         @Override
         public int getCount() {
-            return NUM_PAGES;
+            return videos.length;
         }
 
         @Override
-        public CharSequence getPageTitle (int position) {
-            return "Page " +position;
+        public CharSequence getPageTitle(int position) {
+            return "Page " + position;
         }
     }
+}
+interface VideoListener {
+    void ready();
+    void pause();
+    void resume();
 }
--- core-sample-app/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/sampleapp/examples/viewPagerExample/ViewPagerFragment.java
+++ core-sample-app/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/sampleapp/examples/viewPagerExample/ViewPagerFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
@@ -16,24 +17,51 @@ import androidx.fragment.app.Fragment;
 
 public class ViewPagerFragment extends Fragment {
 
-    private String videoId = VideoIdsProvider.getNextVideoId();
     private YouTubePlayer initializedYouTubePlayer;
+
+    public static ViewPagerFragment newInstance(String videoId, String videoTitle) {
+        ViewPagerFragment myFragment = new ViewPagerFragment();
+        Bundle args = new Bundle();
+        args.putString("videoId", videoId);
+        args.putString("videoTitle", videoTitle);
+        myFragment.setArguments(args);
+        return myFragment;
+    }
+
+    public String getVideoId() {
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            return arguments.getString("videoId");
+        }
+        return "";
+    }
+
+    public String getVideoTitle() {
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            return arguments.getString("videoTitle");
+        }
+        return "";
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_view_pager, container, false);
 
         YouTubePlayerView youTubePlayerView = view.findViewById(R.id.youtube_player_view);
+        TextView txtTitle = view.findViewById(R.id.txtTitle);
+        txtTitle.setText(getVideoTitle());
 
         getLifecycle().addObserver(youTubePlayerView);
 
         youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
             @Override
             public void onReady(@NonNull YouTubePlayer youTubePlayer) {
-                youTubePlayer.cueVideo(videoId, 0);
+                youTubePlayer.cueVideo(getVideoId(), 0);
                 initializedYouTubePlayer = youTubePlayer;
             }
         });
+
 
         return view;
     }
@@ -42,7 +70,12 @@ public class ViewPagerFragment extends Fragment {
     @Override
     public void setMenuVisibility(final boolean visible) {
         super.setMenuVisibility(visible);
-        if (!visible && initializedYouTubePlayer != null)
-            initializedYouTubePlayer.pause();
+        if (initializedYouTubePlayer != null) {
+            if (visible) {
+                initializedYouTubePlayer.play();
+            } else {
+                initializedYouTubePlayer.pause();
+            }
+        }
     }
 }
